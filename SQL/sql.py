0638ebceff7422d753968dcacd6b6fdab3a1cd40
celine-kanagwa <celinekanagwa@gmail.com>
2022-05-30 17:49:34 +0200

    Data Base

--- SQL/sql.py
+++ SQL/sql.py
@@ -2,15 +2,16 @@ import csv
 from importlib.resources import contents
 from multiprocessing import connection 
 import sqlite3
+from construct import obj_
 import pandas as pd 
 
-columns = ['id','MSIND','','', '']
+columns = ['id','MSIND','engagement','experience','satisfaction']
 
 class SavingToSQL:
     def __init__(self, df:pd.DataFrame):
         #initializing model 
         self.df = df
-        self.connection = sqlite3.connect('Tellcommunication.db')
+        self.connection = sqlite3.connect('Telco.db')
         self.cursor = self.connection.cursor()
         print("Automation in action ")
 
@@ -18,15 +19,16 @@ class SavingToSQL:
     def create_table(self):
         create_table = '''CREATE TABLE User_experience(id INTEGER PRIMARY KEY AUTOIINCREMENT,
                                         MSIND NUMBER NOT NULL,
-                                        ......................,
-                                        ......................,
-                                         ......................,
-                                        )'''
+                                        engagement NUMBER NOT NULL,
+                                        experience NUMBER NOT NULL,
+                                        satisfaction NUMBER NOT NULL,
+                                        )
+                                         '''
 
         print('Table creation in action')
 
         try:
-            self.cursor.excute(create_table)
+            self.cursor.execute(create_table)
 
         except sqlite3.OperationalError:
             print('Table already exist...')
@@ -59,11 +61,16 @@ def fetchall ():
     connection.commit()
     connection.close()
 
-	
+    
 if __name__ == '__main__':
-    file = ''
+    file = '../Data/experience_data.csv'
+    obj = SavingToSQL(file)
+    obj.create_table()
+    obj.csv_to_sql()
+    obj.sql_fetchall()
     fetchall ()
-		
+    obj.sql_close()
+        
 
 
 
